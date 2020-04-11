231b8cb788a4e456ffaa2513c0f176b82e134188
Gabriel Gueiros <gabrielrgueiros@live.com>
2020-04-11 17:28:54 -0300

    adição do footer

--- src/js/activate-buttons/activate-buttons.js
+++ src/js/activate-buttons/activate-buttons.js
@@ -6,8 +6,7 @@ export const activateButtons = () => {
         let flexButtons = document.querySelector('.flex-buttons');
         menuButton.classList.toggle('menu-clicado');
         flexButtons.classList.toggle('isShown');
-        
-        
+
         menuButton.className.includes('fa-times')
             ? menuButton.classList.replace('fa-times', 'fa-bars')
             : menuButton.classList.replace('fa-bars','fa-times')
